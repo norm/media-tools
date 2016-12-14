f2a72849ed314723be7e0c374cf0fb6698f16e05
Mark Norman Francis <norm@201created.com>
2016-12-14 08:48:49 +0000

    Clean up converted video dirs if TRASH_DIR is set
    Once converted, the video file will be moved to TRASH_DIR, any
    poster images (including "poster-resized-*.jpg" because AtomicParsley
    sometimes creates those from the original poster image) are deleted,
    and the directory is removed.

--- tests/00_configuration.sh
+++ tests/00_configuration.sh
@@ -3,7 +3,8 @@
 @test "default configuration" {
     run media show-config
     [ "${lines[0]}" = "CONVERT_DIR=/tmp" ]
-    [ "${lines[1]}" = "TV_BASE=/files/tv" ]
+    [ "${lines[1]}" = "TRASH_DIR=" ]
+    [ "${lines[2]}" = "TV_BASE=/files/tv" ]
 }
 
 @test "file overrides default configuration" {
@@ -11,24 +12,29 @@
 
     run media show-config
     [ "${lines[0]}" = "CONVERT_DIR=/tmp/convert" ]
-    [ "${lines[1]}" = "TV_BASE=/tmp/tv" ]
+    [ "${lines[1]}" = "TRASH_DIR=/tmp/trash" ]
+    [ "${lines[2]}" = "TV_BASE=/tmp/tv" ]
 }
 
 @test "environment overrides default configuration" {
-    export MEDIA_CONVERT_DIR=/tmp/env/convert
-    export MEDIA_TV_BASE=/tmp/env/tv
+    export MEDIA_CONVERT_DIR=/tmp/convert
+    export MEDIA_TRASH_DIR=/tmp/trash
+    export MEDIA_TV_BASE=/tmp/tv
 
     run media show-config
-    [ "${lines[0]}" = "CONVERT_DIR=/tmp/env/convert" ]
-    [ "${lines[1]}" = "TV_BASE=/tmp/env/tv" ]
+    [ "${lines[0]}" = "CONVERT_DIR=/tmp/convert" ]
+    [ "${lines[1]}" = "TRASH_DIR=/tmp/trash" ]
+    [ "${lines[2]}" = "TV_BASE=/tmp/tv" ]
 }
 
 @test "environment overrides file overrides default configuration" {
     export MEDIA_CONFIG=tests/config/media.conf
     export MEDIA_CONVERT_DIR=/tmp/env/convert
+    export MEDIA_TRASH_DIR=/tmp/env/trash
     export MEDIA_TV_BASE=/tmp/env/tv
 
     run media show-config
     [ "${lines[0]}" = "CONVERT_DIR=/tmp/env/convert" ]
-    [ "${lines[1]}" = "TV_BASE=/tmp/env/tv" ]
+    [ "${lines[1]}" = "TRASH_DIR=/tmp/env/trash" ]
+    [ "${lines[2]}" = "TV_BASE=/tmp/env/tv" ]
 }
--- tests/40_add_video.sh
+++ tests/40_add_video.sh
@@ -2,22 +2,29 @@
 
 source bin/media
 
+function count_files_in_dir {
+    find "$1" -type f | wc -l | tr -d ' '
+}
+
+function is_empty {
+    local count=$( count_files_in_dir "$1" )
+    [ $count = 0 ]
+}
+
 
 @test "adds a TV show" {
     export MEDIA_TV_BASE=$( mktemp -d )
 
     # there should be nothing before we begin
-    count=$( find $MEDIA_TV_BASE -type f | wc -l | tr -d ' ' )
-    [ "$count" = 0 ]
+    is_empty "$MEDIA_TV_BASE"
 
     media-add "tests/process/House - 1x01 - Pilot"
 
     # media-add should have installed converted TV
-    count=$( find $MEDIA_TV_BASE -type f | wc -l | tr -d ' ' )
-    local installed="$MEDIA_TV_BASE/House/Season 1/01 Pilot.m4v"
+    [ $( count_files_in_dir "$MEDIA_TV_BASE" ) = 1 ]
 
+    local installed="$MEDIA_TV_BASE/House/Season 1/01 Pilot.m4v"
     [ -f "$installed" ]
-    [ "$count" = 1 ]
     [ "$( media_lookup_atom "$installed" tvsh )" = 'House' ]
     [ "$( media_lookup_atom "$installed" tvsn )" = '1' ]
     [ "$( media_lookup_atom "$installed" tves )" = '1' ]
@@ -27,4 +34,40 @@ source bin/media
     [ "$( media_lookup_atom "$installed" covr )" = '1 piece of artwork' ]
 
     rm -rf $MEDIA_TV_BASE
+}
+
+@test "cleans up directories when TRASH_DIR set" {
+    export MEDIA_IGNORE_ITUNES=1
+    export MEDIA_TV_BASE=$( mktemp -d )
+    export MEDIA_TRASH_DIR=$( mktemp -d )
+    local convert_dir=$( mktemp -d )
+
+    # there should be nothing before we begin
+    is_empty "$MEDIA_TV_BASE"
+    is_empty "$MEDIA_TRASH_DIR"
+    is_empty "$convert_dir"
+
+    local source="$convert_dir/House - 1x01 - Pilot"
+    mkdir "$source"
+    cp tests/source/tiny.mp4 "$source"
+    cp tests/source/tv.jpg "$source/poster.jpg"
+    cp tests/source/tv.jpg "$source/poster-resized-4482.jpg"
+
+    media-add "$source"
+
+    [ $( count_files_in_dir "$MEDIA_TV_BASE" ) = 1 ]
+    [ $( count_files_in_dir "$MEDIA_TRASH_DIR" ) = 1 ]
+    is_empty "$convert_dir"
+
+    local installed="$MEDIA_TV_BASE/House/Season 1/01 Pilot.m4v"
+    [ -f "$installed" ]
+    [ "$( media_lookup_atom "$installed" tvsh )" = 'House' ]
+    [ "$( media_lookup_atom "$installed" tvsn )" = '1' ]
+    [ "$( media_lookup_atom "$installed" tves )" = '1' ]
+    [ "$( media_lookup_atom "$installed" tven )" = '1x01' ]
+    [ "$( media_lookup_atom "$installed" ©nam )" = 'Pilot' ]
+    [ "$( media_lookup_atom "$installed" stik )" = 'TV Show' ]
+    [ "$( media_lookup_atom "$installed" covr )" = '1 piece of artwork' ]
+
+    rm -rf $MEDIA_TV_BASE $MEDIA_TRASH_DIR $convert_dir
 }
