316e8c215636ee8ad60bcaba9f916eea55c8e0af
Mark Norman Francis <norm@201created.com>
2016-12-17 12:14:52 +0000

    Stop `media-install` from overwriting videos
    Rather than assuming the generated filename doesn't exist and blindly
    moving the new video to that location, test first and add a number to
    the filename (eg `04 Show.m4v` becomes `04 Show.1.m4v`) until it is an
    unused filename.
    
    Yes, there is theoretically a race condition here. No, I don't think it
    matters in practice.

--- tests/40_add_video.sh
+++ tests/40_add_video.sh
@@ -2,6 +2,14 @@
 
 source bin/media
 
+function count_atoms_in_file {
+    local -r input="$1"
+
+    AtomicParsley "$input" -t \
+        | wc -l \
+        | tr -d ' '
+}
+
 function count_files_in_dir {
     find "$1" -type f \
         | wc -l \
@@ -72,4 +80,38 @@ function is_empty {
     [ "$( media_lookup_atom "$installed" covr )" = '1 piece of artwork' ]
 
     rm -rf $MEDIA_TV_BASE $MEDIA_TRASH_DIR $convert_dir
+}
+
+@test "doesn't overwrite video" {
+    export MEDIA_IGNORE_ITUNES=1
+    export MEDIA_TV_BASE=$( mktemp -d )
+
+    # there should be nothing before we begin
+    is_empty "$MEDIA_TV_BASE"
+
+    # copy a file to the expected destination
+    local -r install_dir="$MEDIA_TV_BASE/House/Season 1"
+    local -r existing="$install_dir/01 Pilot.m4v"
+    mkdir -p "$install_dir"
+    touch "$existing"
+
+    media-add "tests/process/House - 1x01 - Pilot"
+
+    # media-add should have installed converted TV
+    [ "$( count_files_in_dir "$MEDIA_TV_BASE" )" = 2 ]
+
+    # file is not the output video
+    [ "$(count_atoms_in_file "$existing" )" == 0 ]
+
+    local -r installed="$MEDIA_TV_BASE/House/Season 1/01 Pilot.1.m4v"
+    [ -f "$installed" ]
+    [ "$( media_lookup_atom "$installed" tvsh )" = 'House' ]
+    [ "$( media_lookup_atom "$installed" tvsn )" = '1' ]
+    [ "$( media_lookup_atom "$installed" tves )" = '1' ]
+    [ "$( media_lookup_atom "$installed" tven )" = '1x01' ]
+    [ "$( media_lookup_atom "$installed" ©nam )" = 'Pilot' ]
+    [ "$( media_lookup_atom "$installed" stik )" = 'TV Show' ]
+    [ "$( media_lookup_atom "$installed" covr )" = '1 piece of artwork' ]
+
+    rm -rf $MEDIA_TV_BASE
 }
