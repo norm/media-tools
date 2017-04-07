63f2b3af479e11362a97fa94a62c48ebb068de73
Mark Norman Francis <norm@201created.com>
2017-04-07 12:03:26 +0100

    Add track number as an argument to convert-video
    Single video files only have the one track, but as we add DVD support,
    we'll need to be able to convert tracks other than the first.

--- tests/10_convert_video.sh
+++ tests/10_convert_video.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bats
 
+source bin/media
+
+
 function setup {
     OUTPUT_FILE=$( mktemp )
     SOURCE_METADATA=$( mktemp )
@@ -10,33 +13,36 @@ function teardown {
     rm -f "$VIDEO_OUTPUT"  "$SOURCE_METADATA" "$OUTPUT_METADATA"
 }
 
-function get_video_metadata {
+function capture_metadata {
     HandBrakeCLI -i "$SOURCE_FILE" -t0 > "$SOURCE_METADATA" 2>&1
     HandBrakeCLI -i "$OUTPUT_FILE" -t0 > "$OUTPUT_METADATA" 2>&1
 }
 
-function compare_output_size {
-    local -r source_fact=$( awk '/size/ { print $3 }' "$SOURCE_METADATA" )
-    local -r output_fact=$( awk '/size/ { print $3 }' "$OUTPUT_METADATA" )
+function compare_metadata_fact {
+    local fact="$1"
 
-    # conversion should keep size intact
+    local -r source_fact=$(
+        cat "$SOURCE_METADATA" \
+            | extract_track_metadata "$TRACK" \
+            | awk "/$fact/"' { print $3 }'
+    )
+    local -r output_fact=$(
+        cat "$OUTPUT_METADATA" \
+            | extract_track_metadata 1 \
+            | awk "/$fact/"' { print $3 }'
+    )
+
+    echo "source_fact=$source_fact"
+    echo "output_fact=$output_fact"
     [ -n "$source_fact" -a "$source_fact" == "$output_fact" ]
 }
-
-function compare_output_duration {
-    local -r source_fact=$( awk '/duration/ { print $3 }' "$SOURCE_METADATA" )
-    local -r output_fact=$( awk '/duration/ { print $3 }' "$OUTPUT_METADATA" )
-
-    # conversion should keep duration intact
-    [ -n "$source_fact" -a "$source_fact" == "$output_fact" ]
-}
-
 
 @test "converts 720p AC3" {
     SOURCE_FILE="tests/source/720p-ac3.vob"
+    TRACK=1
 
-    run media-convert-video "$SOURCE_FILE" "$OUTPUT_FILE"
-    get_video_metadata
-    compare_output_size
-    compare_output_duration
+    run media-convert-video "$SOURCE_FILE" "$OUTPUT_FILE" "$TRACK"
+    capture_metadata
+    compare_metadata_fact 'size'
+    compare_metadata_fact 'duration'
 }
