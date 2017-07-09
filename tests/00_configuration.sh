41d2f0c9ccae72022a44392be3aaa41d8d4e6204
Mark Norman Francis <norm@201created.com>
2017-07-09 07:18:28 +0100

    Refactor show-config output test
    Adding new lines to the output of `show-config` testing was difficult,
    as they were indexed, so additions would throw out numbering. This
    caused the diffs to be more complex, making spotting what had actually
    changed harder. This fixes that, in preparation for adding new config.

--- tests/00_configuration.sh
+++ tests/00_configuration.sh
@@ -1,56 +1,102 @@
 #!/usr/bin/env bats
 
 @test "default configuration" {
+    local -a expects=(
+        "convert_dir   = /tmp"
+        "ignore_itunes = "
+        "trash_dir     = "
+        "tv_base       = /files/tv"
+    )
+
+    media show-config
     run media show-config
-    [ "${lines[0]}" = "convert_dir   = /tmp" ]
-    [ "${lines[1]}" = "ignore_itunes = " ]
-    [ "${lines[2]}" = "trash_dir     = " ]
-    [ "${lines[3]}" = "tv_base       = /files/tv" ]
+
+    local count=0
+    for line in "${expects[@]}"; do
+        [ "${expects[$count]}" = "${lines[$count]}" ]
+        let count=count+1
+    done
 }
 
 @test "file overrides default configuration" {
-    export MEDIA_CONFIG=tests/config/media.conf
+    local -a expects=(
+        "convert_dir   = /tmp/convert"
+        "ignore_itunes = 1"
+        "trash_dir     = /tmp/trash"
+        "tv_base       = /tmp/tv"
+    )
 
+    export MEDIA_CONFIG=tests/config/media.conf
+    media show-config
     run media show-config
-    [ "${lines[0]}" = "convert_dir   = /tmp/convert" ]
-    [ "${lines[1]}" = "ignore_itunes = 1" ]
-    [ "${lines[2]}" = "trash_dir     = /tmp/trash" ]
-    [ "${lines[3]}" = "tv_base       = /tmp/tv" ]
+
+    local count=0
+    for line in "${expects[@]}"; do
+        [ "${expects[$count]}" = "${lines[$count]}" ]
+        let count=count+1
+    done
 }
 
 @test "partial file doesn't override entire default configuration" {
-    export MEDIA_CONFIG=tests/config/partial.conf
+    local -a expects=(
+        "convert_dir   = /tmp/convert"
+        "ignore_itunes = "
+        "trash_dir     = "
+        "tv_base       = /files/tv"
+    )
 
+    export MEDIA_CONFIG=tests/config/partial.conf
+    media show-config
     run media show-config
-    [ "${lines[0]}" = "convert_dir   = /tmp/convert" ]
-    [ "${lines[1]}" = "ignore_itunes = " ]
-    [ "${lines[2]}" = "trash_dir     = " ]
-    [ "${lines[3]}" = "tv_base       = /files/tv" ]
+
+    local count=0
+    for line in "${expects[@]}"; do
+        [ "${expects[$count]}" = "${lines[$count]}" ]
+        let count=count+1
+    done
 }
 
 @test "environment overrides default configuration" {
+    local -a expects=(
+        "convert_dir   = /tmp/convert"
+        "ignore_itunes = 1"
+        "trash_dir     = /tmp/trash"
+        "tv_base       = /tmp/tv"
+    )
+
     export MEDIA_CONVERT_DIR=/tmp/convert
     export MEDIA_IGNORE_ITUNES=1
     export MEDIA_TRASH_DIR=/tmp/trash
     export MEDIA_TV_BASE=/tmp/tv
-
+    media show-config
     run media show-config
-    [ "${lines[0]}" = "convert_dir   = /tmp/convert" ]
-    [ "${lines[1]}" = "ignore_itunes = 1" ]
-    [ "${lines[2]}" = "trash_dir     = /tmp/trash" ]
-    [ "${lines[3]}" = "tv_base       = /tmp/tv" ]
+
+    local count=0
+    for line in "${expects[@]}"; do
+        [ "${expects[$count]}" = "${lines[$count]}" ]
+        let count=count+1
+    done
 }
 
 @test "environment overrides file overrides default configuration" {
+    local -a expects=(
+        "convert_dir   = /tmp/env/convert"
+        "ignore_itunes = 2"
+        "trash_dir     = /tmp/env/trash"
+        "tv_base       = /tmp/env/tv"
+    )
+
     export MEDIA_CONFIG=tests/config/media.conf
     export MEDIA_CONVERT_DIR=/tmp/env/convert
     export MEDIA_IGNORE_ITUNES=2
     export MEDIA_TRASH_DIR=/tmp/env/trash
     export MEDIA_TV_BASE=/tmp/env/tv
-
+    media show-config
     run media show-config
-    [ "${lines[0]}" = "convert_dir   = /tmp/env/convert" ]
-    [ "${lines[1]}" = "ignore_itunes = 2" ]
-    [ "${lines[2]}" = "trash_dir     = /tmp/env/trash" ]
-    [ "${lines[3]}" = "tv_base       = /tmp/env/tv" ]
+
+    local count=0
+    for line in "${expects[@]}"; do
+        [ "${expects[$count]}" = "${lines[$count]}" ]
+        let count=count+1
+    done
 }
