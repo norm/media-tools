c3df6d8d876ab534868c747e4ba1e640369702f8
Mark Norman Francis <norm@201created.com>
2017-07-16 21:49:55 +0100

    Change how to specify the title of a YT playlist
    Actual usage reads better to make specifying the title of a YouTube
    playlist an option (--title) rather than an optional second argument.

--- tests/20_get_youtube_playlist.sh
+++ tests/20_get_youtube_playlist.sh
@@ -149,6 +149,57 @@ PLAYLIST=PL4zR2yLTCZ8chNFr0T_5EYzbCuNwaSRgO
     rm -rf "$MEDIA_CACHE_DIR" "$MEDIA_CONVERT_DIR"
 }
 
+@test "downloads with a custom title" {
+    declare -a VIDEOS=(
+        ppqiWVaO3rg
+        EyI-5e1IrEo
+        BE7gAEsI0XY
+    )
+
+    export MEDIA_CACHE_DIR=$( mktemp -d )
+    export MEDIA_CONVERT_DIR=$( mktemp -d )
+
+    dir_is_empty "$MEDIA_CACHE_DIR"
+    dir_is_empty "$MEDIA_CONVERT_DIR"
+
+    local -r cache_dir="$MEDIA_CACHE_DIR/$PLAYLIST"
+    local -r convert_dir="$MEDIA_CONVERT_DIR/$PLAYLIST"
+
+    media-get-youtube-playlist \
+        --title 'Testing Playlist' \
+        --reversed \
+        --season 1 \
+            $PLAYLIST
+
+    [ $( count_files_in_dir "$MEDIA_CACHE_DIR" ) = 5 ]
+    [ $( count_files_in_dir "$MEDIA_CONVERT_DIR" ) = 9 ]
+    [ -f "$cache_dir/playlist.json" ]
+    [ -f "$cache_dir/season_1.count" ]
+
+    # each video in the playlist has...
+    for video in "${VIDEOS[@]}"; do
+        # source files suitable for converting,
+        [ -f "$convert_dir/$video/$video.mp4" ]
+        [ -f "$convert_dir/$video/metadata.conf" ]
+        [ -f "$convert_dir/$video/poster.jpg" ]
+
+        grep -q 'series = Testing Playlist' \
+            "$convert_dir/$video/metadata.conf"
+
+        # and the original fetch JSON metadata stored for later use
+        [ -f "$cache_dir/$video.info.json" ]
+    done
+
+    grep -q 'season = 1' "$convert_dir/ppqiWVaO3rg/metadata.conf"
+    grep -q 'episode = 01' "$convert_dir/ppqiWVaO3rg/metadata.conf"
+    grep -q 'season = 1' "$convert_dir/EyI-5e1IrEo/metadata.conf"
+    grep -q 'episode = 02' "$convert_dir/EyI-5e1IrEo/metadata.conf"
+    grep -q 'season = 1' "$convert_dir/BE7gAEsI0XY/metadata.conf"
+    grep -q 'episode = 03' "$convert_dir/BE7gAEsI0XY/metadata.conf"
+
+    rm -rf "$MEDIA_CACHE_DIR" "$MEDIA_CONVERT_DIR"
+}
+
 @test "downloads nothing when cached files exist" {
     declare -a VIDEOS=(
         ppqiWVaO3rg
