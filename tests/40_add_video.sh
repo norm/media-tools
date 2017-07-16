9f457411e90508dcf67b57bcd951cb8ae0ea30ab
Mark Norman Francis <norm@201created.com>
2017-07-16 17:17:04 +0100

    Test that a metadata file overrides directory name

--- tests/40_add_video.sh
+++ tests/40_add_video.sh
@@ -156,3 +156,37 @@ source tests/lib.sh
 
     rm -rf $MEDIA_TV_BASE
 }
+
+@test "metadata overrides directory name for single video file directories" {
+    export MEDIA_IGNORE_ITUNES=1
+    export MEDIA_TV_BASE=$( mktemp -d )
+    local -r process_dir=$( mktemp -d )
+
+    # there should be nothing before we begin
+    dir_is_empty "$MEDIA_TV_BASE"
+    dir_is_empty "$process_dir"
+
+    local -r source_dir="$process_dir/House - 1x01 - Everybody Lies"
+    mkdir -p "$source_dir"
+    cp tests/source/tiny.mp4 "$source_dir/tiny.mp4"
+    cp tests/source/tv.jpg "$source_dir/poster.jpg"
+    cp tests/config/episode.conf "$source_dir/metadata.conf"
+
+    media-add-video "$source_dir"
+
+    # media-add should have installed converted TV
+    [ $( count_files_in_dir "$MEDIA_TV_BASE" ) = 1 ]
+
+    local installed="$MEDIA_TV_BASE/Buffy the Vampire Slayer/Season 4/09 Something Blue.m4v"
+    [ -f "$installed" ]
+    media_lookup_atom "$installed" .
+    [ "$( media_lookup_atom "$installed" tvsh )" = 'Buffy the Vampire Slayer' ]
+    [ "$( media_lookup_atom "$installed" tvsn )" = '4' ]
+    [ "$( media_lookup_atom "$installed" tves )" = '9' ]
+    [ "$( media_lookup_atom "$installed" tven )" = '4x09' ]
+    [ "$( media_lookup_atom "$installed" ©nam )" = 'Something Blue' ]
+    [ "$( media_lookup_atom "$installed" stik )" = 'TV Show' ]
+    [ "$( media_lookup_atom "$installed" covr )" = '1 piece of artwork' ]
+
+    # rm -rf $MEDIA_TV_BASE
+}
