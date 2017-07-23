701ba4810880f51f6d9ec5a9ecc6610720f464fd
Mark Norman Francis <norm@201created.com>
2017-07-23 10:07:38 +0100

    Allow some HandBrake settings to be overridden
    The metadata file can contain `maxWidth`, `maxHeight`,
    `crop`, and `decomb` values to override the default
    HandBrake settings chosen by media-tools.

--- tests/10_handbrake_arguments.sh
+++ tests/10_handbrake_arguments.sh
@@ -39,6 +39,50 @@ source bin/media-convert-video
     done
 }
 
+@test "override settings in metadata" {
+    local -r input_dir=$( mktemp -d )
+    local -r track=1
+
+    cp tests/source/720p-ac3.vob "$input_dir"
+    cat > "$input_dir/metadata.conf" <<EOF
+maxWidth = 1280
+maxHeight = 720
+crop = 10:10:10:10
+EOF
+
+    local -a args=( $(get_handbrake_args "$input_dir/720p-ac3.vob" "$track") )
+
+    local -ar expected=(
+        --audio 1
+        --ab 640
+        --mixdown 6ch
+        --arate Auto
+        --aencoder copy:ac3
+        --loose-anamorphic
+        --modulus 2
+        --crop 10:10:10:10
+        --decomb fast
+        --rate 30
+        --pfr
+        --format mp4
+        --encoder x264
+        --encopts b-adapt=2
+        --markers
+        --maxWidth 1280
+        --maxHeight 720
+        --quality 22
+        --x264-preset slow
+        --h264-profile high
+        --h264-level 4.1
+    )
+
+    echo "expected = ${expected[@]}"
+    echo "got      = ${args[@]}"
+    for i in $(seq 0 "${#expected[@]}"); do
+        [ "${expected[$i]}" == "${args[$i]}" ]
+    done
+}
+
 @test "testing video conversion" {
     export MEDIA_TESTING=1
     local -r input=tests/source/720p-ac3.vob
