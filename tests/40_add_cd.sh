ba8022e624d66abf5b028ba29b89c8b165c93ca7
Mark Norman Francis <norm@201created.com>
2017-08-05 09:16:16 +0100

    Add the album artist to converted CDs

--- tests/40_add_cd.sh
+++ tests/40_add_cd.sh
@@ -4,18 +4,13 @@ source bin/media
 source tests/lib.sh
 
 
-function setup {
-    export MEDIA_CD_RIP_DIR=$( mktemp -d )
-    export MEDIA_AUTO_ADD_DIR=$( mktemp -d )
-}
-
-function teardown {
-    rm -rf "$MEDIA_CD_RIP_DIR" "$MEDIA_AUTO_ADD_DIR"
-}
-
-
 @test "add a CD" {
     needs_source tests/source/39038f04
+
+    export MEDIA_CD_RIP_DIR=$( mktemp -d )
+    export MEDIA_AUTO_ADD_DIR=$( mktemp -d )
+    echo "** rip_dir=$MEDIA_CD_RIP_DIR"
+    echo "** add_dir=$MEDIA_AUTO_ADD_DIR"
 
     rsync -a --delete tests/source/39038f04 "$MEDIA_CD_RIP_DIR"
 
@@ -35,23 +30,38 @@ function teardown {
     [ -f "$track" ]
     [ "$( media_lookup_atom "$track" trkn )" = '1 of 4' ]
     [ "$( media_lookup_atom "$track" ©nam )" = 'Dignity' ]
+    [ "$( media_lookup_atom "$track" aART )" = 'Deacon Blue' ]
+    [ "$( media_lookup_atom "$track" ©ART )" = 'Deacon Blue' ]
     [ "$( media_lookup_atom "$track" stik )" = 'Normal' ]
+    [ $( count_atoms_in_file "$track" ) = 12 ]
 
     track="$MEDIA_AUTO_ADD_DIR/02.m4a"
     [ -f "$track" ]
     [ "$( media_lookup_atom "$track" trkn )" = '2 of 4' ]
     [ "$( media_lookup_atom "$track" ©nam )" = 'Beautiful Stranger' ]
+    [ "$( media_lookup_atom "$track" aART )" = 'Deacon Blue' ]
+    [ "$( media_lookup_atom "$track" ©ART )" = 'Deacon Blue' ]
     [ "$( media_lookup_atom "$track" stik )" = 'Normal' ]
+    [ $( count_atoms_in_file "$track" ) = 12 ]
 
     track="$MEDIA_AUTO_ADD_DIR/03.m4a"
     [ -f "$track" ]
     [ "$( media_lookup_atom "$track" trkn )" = '3 of 4' ]
     [ "$( media_lookup_atom "$track" ©nam )" = 'Waves of sorrow (Piano and vocal version)' ]
+    [ "$( media_lookup_atom "$track" aART )" = 'Deacon Blue' ]
+    [ "$( media_lookup_atom "$track" ©ART )" = 'Deacon Blue' ]
     [ "$( media_lookup_atom "$track" stik )" = 'Normal' ]
+    [ $( count_atoms_in_file "$track" ) = 12 ]
 
     track="$MEDIA_AUTO_ADD_DIR/04.m4a"
     [ -f "$track" ]
     [ "$( media_lookup_atom "$track" trkn )" = '4 of 4' ]
     [ "$( media_lookup_atom "$track" ©nam )" = "Bethelehem's gate (Piano and vocal version)" ]
+    [ "$( media_lookup_atom "$track" aART )" = 'Deacon Blue' ]
+    [ "$( media_lookup_atom "$track" ©ART )" = 'Deacon Bloo' ]
+    [ "$( media_lookup_atom "$track" ©cmt )" = 'Really by Deacon Blue' ]
     [ "$( media_lookup_atom "$track" stik )" = 'Normal' ]
+    [ $( count_atoms_in_file "$track" ) = 13 ]
+
+    rm -rf "$MEDIA_CD_RIP_DIR" "$MEDIA_AUTO_ADD_DIR"
 }
