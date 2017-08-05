b79b11375e9cce69e43e6678513f8f2c6b1e396d
Mark Norman Francis <norm@201created.com>
2017-08-05 08:52:09 +0100

    Clean up resized artwork artefacts
    Sometimes AtomicParsley creates temporary "file-resized-243.jpg" artefacts when adding artwork. Make sure they are cleaned up.

--- tests/10_set_artwork.sh
+++ tests/10_set_artwork.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bats
 
 source bin/media
+source tests/lib.sh
 
 
 @test "add one poster" {
@@ -26,4 +27,24 @@ source bin/media
     [ "$(media_lookup_atom "$source" covr)" == '2 pieces of artwork' ]
 
     rm -f "$source"
+}
+
+@test "add one poster that triggers resized temp file" {
+    local -r source="$( mktemp -d )"
+    echo "** source_dir=$source"
+
+    cp tests/source/tiny.mp4 "$source"
+    cp tests/inputs/resizes.jpg "$source/cover.jpg"
+    [ "$(media_lookup_atom "$source/tiny.mp4" covr)" == '' ]
+
+    # only the files we've copied in
+    [ $( count_files_in_dir "$source" ) = 2 ]
+
+    media-set-artwork "$source/tiny.mp4" "$source/cover.jpg"
+    [ "$(media_lookup_atom "$source/tiny.mp4" covr)" == '1 piece of artwork' ]
+
+    # only the files we've copied in
+    [ $( count_files_in_dir "$source" ) = 2 ]
+
+    rm -rf "$source"
 }
