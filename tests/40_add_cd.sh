2a37c602c33c758ffecfc7deb29cc8e987498983
Mark Norman Francis <norm@201created.com>
2017-10-16 07:08:26 +0100

    Handle CD titles containing double quotes
    They weren't being escaped, and so they were causing trouble.

--- tests/40_add_cd.sh
+++ tests/40_add_cd.sh
@@ -65,3 +65,42 @@ source tests/lib.sh
 
     rm -rf "$MEDIA_CD_RIP_DIR" "$MEDIA_AUTO_ADD_DIR"
 }
+
+@test "add a mixed-media CD" {
+    needs_source tests/source/tomb_raider
+
+    export MEDIA_CD_RIP_DIR=$( mktemp -d )
+    export MEDIA_AUTO_ADD_DIR=$( mktemp -d )
+    echo "** rip_dir=$MEDIA_CD_RIP_DIR"
+    echo "** add_dir=$MEDIA_AUTO_ADD_DIR"
+
+    rsync -a --delete tests/source/tomb_raider "$MEDIA_CD_RIP_DIR"
+
+    find "$MEDIA_CD_RIP_DIR" -type f
+
+    # should have copied seven files (5 wav, 1 metadata.conf, 1 cover.jpg)
+    [ $( count_files_in_dir "$MEDIA_CD_RIP_DIR" ) = 7 ]
+
+    # should not be any files in auto-add dir
+    dir_is_empty "$MEDIA_AUTO_ADD_DIR"
+
+    media-add-cd tomb_raider
+
+    # should now have five output files
+    [ $( count_files_in_dir "$MEDIA_AUTO_ADD_DIR" ) = 5 ]
+
+    local track
+    track="$MEDIA_AUTO_ADD_DIR/01.m4a"
+    [ -f "$track" ]
+    [ "$( media_lookup_atom "$track" trkn )" = '1 of 5' ]
+    [ "$( media_lookup_atom "$track" ©nam )" = '"Here lies Tahocan…"' ]
+    [ "$( media_lookup_atom "$track" aART )" = 'Nathan McCree' ]
+    [ "$( media_lookup_atom "$track" ©ART )" = 'Nathan McCree' ]
+    [ "$( media_lookup_atom "$track" stik )" = 'Normal' ]
+    [ "$( media_lookup_atom "$track" gnre )" = 'Game' ]
+    [ "$( media_lookup_atom "$track" purd )" = '1996-10-25T12:00:00Z' ]
+    [ "$( media_lookup_atom "$track" ©day )" = '1996-10-25T12:00:00Z' ]
+    [ $( count_atoms_in_file "$track" ) = 13 ]
+
+    rm -rf "$MEDIA_CD_RIP_DIR" "$MEDIA_AUTO_ADD_DIR"
+}
